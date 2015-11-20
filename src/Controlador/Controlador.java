187f1a4b9c3e697b03515cab46b2c9c85b92b4a1
Mantenimiento <Mantenimiento@PC16_AULA2.nervion.local>
2015-11-20 10:54:10 +0100

    Primeras líneas del contorlador ---Jessica---

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -1,27 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controlador;
 
+import Modelo.Modelo;
 import Vistas.Vista_Principal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- *
- * @author MrDrulix
- */
-public class Controlador {
+public class Controlador implements ActionListener{
     
 Vista_Principal v;
+Modelo m = new Modelo();
 
     public Controlador(Vista_Principal vista) {
       this.v=vista;
     }
 
+    
+
+    public enum Acciones{
+        principalCargarTablaArticulos
+    }
+    
     public void iniciarMain() {
         this.v.setVisible(true);
         
+        this.v.tbl_Tabla_Principal.setModel(this.m.getTableModel("articulo"));
+        
+        this.v.btn_principal_articulos.setActionCommand("principalCargarTablaArticulos");
+        this.v.btn_principal_articulos.addActionListener(this);
+        
     }
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch(Acciones.valueOf(e.getActionCommand())){
+            case principalCargarTablaArticulos:
+                
+                break;
+        }
+    }
+      
     
 }
